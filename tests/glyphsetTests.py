4d090883dc6c9a4e64cc6dc35ef2dc6b5c0e72c9
Joseph Cottam <jcottam@indiana.edu>
2014-07-17 23:00:52 -0400

    Formatting fixes

--- tests/glyphsetTests.py
+++ tests/glyphsetTests.py
@@ -11,27 +11,27 @@ class UtilTests(unittest.TestCase):
         self.assertEqual(val, f(object()), "Object (%s)" % condition)
         self.assertEqual(val, f(None), "None (%s)" % condition)
 
-  def test_const(self):
-      f = glyphset.const(3)
-      self._consts(f, "Number", 3)
+    def test_const(self):
+        f = glyphset.const(3)
+        self._consts(f, "Number", 3)
 
-    f = glyphset.const("two")
-    self._consts(f, "String", "two")
+        f = glyphset.const("two")
+        self._consts(f, "String", "two")
 
-    f = glyphset.const(None)
-    self._consts(f, "None", None)
+        f = glyphset.const(None)
+        self._consts(f, "None", None)
 
-    o = object()
-    f = glyphset.const(o)
-    self._consts(f, "Object", o)
+        o = object()
+        f = glyphset.const(o)
+        self._consts(f, "Object", o)
 
 
-  def test_idx(self):
-      f = glyphset.idx(2)
-      self.assertEquals(2, f([0,1,2,3,4]))
-      self.assertEquals(20, f([0,10,20,30,40]))
-      self.assertIsNone(f([0,1,None,3,4]), "None from list")
-      self.assertEquals(3, f(xrange(1,20)), "Function source")
+    def test_idx(self):
+        f = glyphset.idx(2)
+        self.assertEquals(2, f([0,1,2,3,4]))
+        self.assertEquals(20, f([0,10,20,30,40]))
+        self.assertIsNone(f([0,1,None,3,4]), "None from list")
+        self.assertEquals(3, f(xrange(1,20)), "Function source")
 
 class ShaperTests(unittest.TestCase):
     def test_Literals(self):
@@ -39,57 +39,57 @@ class ShaperTests(unittest.TestCase):
         l = [1,2,3,4]
         self.assertIs(f(l),l)
 
-    o = object()
-    self.assertIs(f(o),o)
-    self.assertIs(f(f),f)
-    self.assertIs(f(0),0)
+        o = object()
+        self.assertIs(f(o),o)
+        self.assertIs(f(f),f)
+        self.assertIs(f(0),0)
 
-  def test_ToRect(self):
-      f = glyphset.ToRect(glyphset.idx(3), glyphset.idx(2), glyphset.idx(1), glyphset.idx(0))
-      self.assertEquals(f([[1,2,3,4,5,6]]), [[4,3,2,1]], "Lists are indexable")
-      self.assertEquals(f(["ABCDEF"]), [["D","C","B","A"]], "Strings are indexable")
-      self.assertEquals(f(["ABCDEF","abcdef"]), [["D","C","B","A"],["d","c","b","a"]], "multiple items")
+    def test_ToRect(self):
+        f = glyphset.ToRect(glyphset.idx(3), glyphset.idx(2), glyphset.idx(1), glyphset.idx(0))
+        self.assertEquals(f([[1,2,3,4,5,6]]), [[4,3,2,1]], "Lists are indexable")
+        self.assertEquals(f(["ABCDEF"]), [["D","C","B","A"]], "Strings are indexable")
+        self.assertEquals(f(["ABCDEF","abcdef"]), [["D","C","B","A"],["d","c","b","a"]], "multiple items")
 
-    f = glyphset.ToRect(glyphset.idx(2), glyphset.idx(4), glyphset.idx(1), glyphset.idx(1))
-    self.assertEquals(f([[1,2,3,4,5,6]]), [[3,5,2,2]])
-    self.assertEquals(f(["ABCDEF"]), [["C","E","B","B"]])
+        f = glyphset.ToRect(glyphset.idx(2), glyphset.idx(4), glyphset.idx(1), glyphset.idx(1))
+        self.assertEquals(f([[1,2,3,4,5,6]]), [[3,5,2,2]])
+        self.assertEquals(f(["ABCDEF"]), [["C","E","B","B"]])
 
-  def test_ToLine(self):
-      f = glyphset.ToLine(glyphset.idx(3), glyphset.idx(2), glyphset.idx(1), glyphset.idx(0))
-      self.assertEquals(f([[1,2,3,4,5,6]]), [[4,3,2,1]], "Lists are indexable")
-      self.assertEquals(f(["ABCDEF"]), [["D","C","B","A"]], "Strings are indexable")
-      self.assertEquals(f(["ABCDEF","abcdef"]), [["D","C","B","A"],["d","c","b","a"]], "multiple items")
+    def test_ToLine(self):
+        f = glyphset.ToLine(glyphset.idx(3), glyphset.idx(2), glyphset.idx(1), glyphset.idx(0))
+        self.assertEquals(f([[1,2,3,4,5,6]]), [[4,3,2,1]], "Lists are indexable")
+        self.assertEquals(f(["ABCDEF"]), [["D","C","B","A"]], "Strings are indexable")
+        self.assertEquals(f(["ABCDEF","abcdef"]), [["D","C","B","A"],["d","c","b","a"]], "multiple items")
 
-    f = glyphset.ToLine(glyphset.idx(2), glyphset.idx(4), glyphset.idx(1), glyphset.idx(1))
-    self.assertEquals(f([[1,2,3,4,5,6]]), [[3,5,2,2]])
-    self.assertEquals(f(["ABCDEF"]), [["C","E","B","B"]])
+        f = glyphset.ToLine(glyphset.idx(2), glyphset.idx(4), glyphset.idx(1), glyphset.idx(1))
+        self.assertEquals(f([[1,2,3,4,5,6]]), [[3,5,2,2]])
+        self.assertEquals(f(["ABCDEF"]), [["C","E","B","B"]])
 
 
-  def test_ToPoint(self):
-      f = glyphset.ToPoint(glyphset.idx(3), glyphset.idx(2))
-      self.assertEquals(f([[1,2,3,4,5,6]]), [[4,3,0,0]], "Lists are indexable")
-      self.assertEquals(f(["ABCDEF"]), [["D","C",0,0]], "Strings are indexable")
-      self.assertEquals(f(["ABCDEF","abcdef"]), [["D","C",0,0],["d","c",0,0]], "multiple items")
+    def test_ToPoint(self):
+        f = glyphset.ToPoint(glyphset.idx(3), glyphset.idx(2))
+        self.assertEquals(f([[1,2,3,4,5,6]]), [[4,3,0,0]], "Lists are indexable")
+        self.assertEquals(f(["ABCDEF"]), [["D","C",0,0]], "Strings are indexable")
+        self.assertEquals(f(["ABCDEF","abcdef"]), [["D","C",0,0],["d","c",0,0]], "multiple items")
 
-    f = glyphset.ToPoint(glyphset.idx(2), glyphset.idx(4))
-    self.assertEquals(f([[1,2,3,4,5,6]]), [[3,5,0,0]])
-    self.assertEquals(f(["ABCDEF"]), [["C","E",0,0]])
+        f = glyphset.ToPoint(glyphset.idx(2), glyphset.idx(4))
+        self.assertEquals(f([[1,2,3,4,5,6]]), [[3,5,0,0]])
+        self.assertEquals(f(["ABCDEF"]), [["C","E",0,0]])
 
 
 class GlyphsetTests(unittest.TestCase):
     _glyphset = None
 
-  def test_bounds(self):
-      if not self._glyphset: return
-      self.assertTrue(np.array_equal(self._glyphset.bounds(), self._bounds))
+    def test_bounds(self):
+        if not self._glyphset: return
+        self.assertTrue(np.array_equal(self._glyphset.bounds(), self._bounds))
 
-  def test_points(self):
-      if not self._glyphset: return
-      self.assertTrue(np.array_equal(self._glyphset.points(), self._points))
+    def test_points(self):
+        if not self._glyphset: return
+        self.assertTrue(np.array_equal(self._glyphset.points(), self._points))
 
-  def test_data(self):
-      if not self._glyphset: return
-      self.assertEquals(self._glyphset.data(), self._data)
+    def test_data(self):
+        if not self._glyphset: return
+        self.assertEquals(self._glyphset.data(), self._data)
 
 class ArrayGlyphset(GlyphsetTests):
     def setUp(self):
