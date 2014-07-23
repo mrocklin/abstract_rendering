02367a2edfd475decb6fc207c864622eaa61acaa
Joseph Cottam <jcottam@indiana.edu>
2014-07-23 18:16:47 -0400

    Formatting...

--- abstract_rendering/general.py
+++ abstract_rendering/general.py
@@ -4,11 +4,12 @@ Operations that apply across a broad variety of categories.
 
 import core
 
+
 class Id(core.Shader):
-    """ Return the input unchanged.  
-     This DOES NOT make a copy.  
+    """ Return the input unchanged.
+     This DOES NOT make a copy.
      It is usually used a zero-cost placeholder.
     """
-    def shade(self, grid):
-        return grid 
 
+    def shade(self, grid):
+        return grid
