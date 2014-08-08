cb3ea97d4babc495557e3c453ea08ecd0b948464
Joseph Cottam <jcottam@indiana.edu>
2014-08-08 11:31:26 -0400

    Updated contour splits and tests.

--- abstract_rendering/contour.py
+++ abstract_rendering/contour.py
@@ -50,9 +50,7 @@ class Contour(core.ShapeShader):
     def nlevels(cls, grid, n):
         "Given grid of values, pick out n values for iso contour levels"
 
-        # The contouring library outlines places below the given value
-        # So we do n+1+1 (aka n+2). The first +1 is because grid.max()
-        # will always result in an empty contour.  The 2nd +1 to get
-        # n lines (aka, n+1 spaces between lines).  
-        # The [:-1] drops the level at grid.max()
-        return np.linspace(grid.min(), grid.max(), n+2)[:-1]
+        # The contouring library outlines places below the given value.
+        # We do n+2 and drop the first and last values because they are
+        # empty contours.
+        return np.linspace(grid.min(), grid.max(), n+2)[1:-1]
--- tests/contourTests.py
+++ tests/contourTests.py
@@ -23,52 +23,52 @@ class ContourTests(unittest.TestCase):
              [0, 0, 0, 0, 0, 0, 0, 0]])
 
     def test_nlevels(self):
-        self.assertListEqual(Contour.nlevels(self.pyramid2, 2).tolist(), [0, 1])
-        self.assertListEqual(Contour.nlevels(self.pyramid2, 3).tolist(), [0, .5, 1])
-        self.assertListEqual(Contour.nlevels(self.pyramid4, 2).tolist(), [0, 3])
-        self.assertListEqual(Contour.nlevels(self.pyramid4, 4).tolist(), [0, 1, 2, 3])
+        self.assertListEqual(Contour.nlevels(self.pyramid2, 1).tolist(), [.5])
+        self.assertListEqual(Contour.nlevels(self.pyramid2, 3).tolist(), [.25, .5, .75])
+        self.assertListEqual(Contour.nlevels(self.pyramid4, 2).tolist(), [1,2])
+        self.assertListEqual(Contour.nlevels(self.pyramid4, 3).tolist(), [.75, 1.5, 2.25])
 
     def test_fuse2(self):
-        c = Contour(levels=2)
+        c = Contour(levels=1)
         contours = c.fuse(self.pyramid2)
 
-        self.assertEquals(len(contours), 2)
-        expected0 = [(1.0, 0.0),
-                     (2.0, 0.0),
-                     (3.0, 1.0),
-                     (3.0, 2.0),
-                     (2.0, 3.0),
-                     (1.0, 3.0),
-                     (0.0, 2.0),
-                     (0.0, 1.0),
-                     (1.0, 0.0)]
+        self.assertEquals(len(contours), 1)
+        expected0 = [(1.0, 0.5), 
+                     (2.0, 0.5), 
+                     (2.5, 1.0),
+                     (2.5, 2.0),
+                     (2.0, 2.5),
+                     (1.0, 2.5),
+                     (0.5, 2.0),
+                     (0.5, 1.0),
+                     (1.0, 0.5)]
 
-        self.assertListEqual(contours[0], expected0)
+        self.assertListEqual(contours[.5], expected0)
 
     def test_fuse4(self):
-        c = Contour(levels=4)
+        c = Contour(levels=3)
         levels = c.fuse(self.pyramid4)
 
-        self.assertEquals(len(levels), 4)
-        self.assertEquals(len(levels[0]), 27)
-        self.assertEquals(len(levels[1]), 19)
-        self.assertEquals(len(levels[2]), 13)
+        self.assertEquals(len(levels), 3)
+        self.assertEquals(len(levels[.75]), 27)
+        self.assertEquals(len(levels[1.5]), 19)
+        self.assertEquals(len(levels[2.25]), 13)
 
-        expected2 = [(3.0, 2.0),
-                     (4.0, 2.0),
-                     (5.0, 2.0),
-                     (5.5, 3.0),
-                     (5.5, 4.0),
-                     (5.5, 5.0),
-                     (5.0, 6.0),
-                     (4.0, 6.0),
-                     (3.0, 6.0),
-                     (2.0, 5.0),
-                     (2.0, 4.0),
-                     (2.0, 3.0),
-                     (3.0, 2.0)]
+        expected2 = [(3.0, 2.25),
+                     (4.0, 2.25),
+                     (5.0, 2.25),
+                     (5.375, 3.0),
+                     (5.375, 4.0),
+                     (5.375, 5.0),
+                     (5.0, 5.75),
+                     (4.0, 5.75),
+                     (3.0, 5.75),
+                     (2.25, 5.0),
+                     (2.25, 4.0),
+                     (2.25, 3.0),
+                     (3.0, 2.25)]
 
-        self.assertListEqual(levels[2], expected2)
+        self.assertListEqual(levels[2.25], expected2)
 
 if __name__ == '__main__':
     unittest.main()
