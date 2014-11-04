1f2c07e5c75c0acca28e5ad198f70c7514d98eaf
Matthew Rocklin <mrocklin@gmail.com>
2014-11-04 12:53:19 -0800

    bin1d docstring

--- abstract_rendering/bin.py
+++ abstract_rendering/bin.py
@@ -7,32 +7,61 @@ import blaze
 from numbers import Number
 
 
-def bin1d(x, binsize, scope=None):
+def bin1d(data, binsize, scope=None):
     """ Bin data by binsize
 
-    >>> x = [1, 2, 1, 5, 6]
-    >>> bin1d(x, 2)
+    Parameters
+    ----------
+
+    data: Anything Blaze understands
+        Array to bin
+    binsize: Number or Blaze function or Blaze expression
+        size of a bin
+
+    Examples
+    --------
+
+    In basic use, binsize can be just the size of a numeric bin.  Here bins are
+    of size two.
+
+    >>> data = [1, 2, 1, 5, 6]
+    >>> bin1d(data, 2)
     (array([0, 2, 4, 6], dtype=int32), array([2, 1, 1, 1], dtype=int32))
 
-    >>> x = [date(2000, 1, 1), date(2001, 2, 2), date(2001, 1, 2)]
-    >>> bin1d(x, blaze.year)
+    Alternatively provide a blaze function.  This function will be applied to
+    control the binning
+
+    >>> data = [date(2000, 1, 1), date(2001, 2, 2), date(2001, 1, 2)]
+    >>> bin1d(data, blaze.year)
     (array([2000, 2001], dtype=int32), array([1, 2], dtype=int32))
+
+    Or, if your data is already an interactive blaze expression
+
+    >>> data = blaze.Data([date(2000, 1, 1), date(2001, 2, 2), date(2001, 1, 2)])
+
+    Then just provide an expression over which to bin
+
+    >>> bin1d(data, data.month)
+    (array([1, 2], dtype=int32), array([2, 1], dtype=int32))
     """
-    if not isinstance(x, Expr):
-        x = Data(x)
+    if not isinstance(data, Expr):
+        data = Data(data)
 
     if (isinstance(binsize, type) and issubclass(binsize, Expr) or
-        callable(binsize) and binsize.__name__ in dir(x)):
+        callable(binsize) and binsize.__name__ in dir(data)):
         f = binsize
-        binsize = f(x)
-        binsexpr = f(x).distinct().sort(binsize._name)
+        binsize = f(data)
+        binsexpr = f(data).distinct().sort(binsize._name)
 
-    if isinstance(binsize, Number) and isnumeric(x.dshape):
+    elif isinstance(binsize, Number) and isnumeric(data.dshape):
         n = binsize
-        binsize = floor(x / n)
+        binsize = floor(data / n)
         binsexpr = floor((binsize * n).distinct().sort(binsize._name))
 
-    assert isinstance(binsize, Expr) and x in binsize
+    elif isinstance(binsize, Expr):
+        binsexpr = binsize.distinct().sort(binsize._name)
+
+    assert isinstance(binsize, Expr) and data in binsize
 
     countsexpr = binsize.count_values().sort(binsize._name).count
 
